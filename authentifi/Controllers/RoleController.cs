6f4594ac100bead3e4b2cb5a5b31dee1aae57564
omarnebi <omar.nebi147@gmail.com>
2024-08-10 17:46:39 +0100

    commit 2

--- authentifi/Controllers/RoleController.cs
+++ authentifi/Controllers/RoleController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
 using authentifi.Models;
+using authentifi.Data;
+using Microsoft.Data.SqlClient;
+using authentifi.DTO;
 
 namespace authentifi.Controllers
 {
@@ -13,14 +16,15 @@ namespace authentifi.Controllers
 	{
 		private readonly RoleManager<IdentityRole> _roleManager;
 		private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
 
-		public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
-		{
-			_roleManager = roleManager;
-			_userManager = userManager;
-		}
-
-		[HttpGet]
+        [HttpGet]
 		public IActionResult GetRoles()
 		{
 			var roles = _roleManager.Roles.ToList();
@@ -72,5 +76,50 @@ namespace authentifi.Controllers
 
 			return BadRequest(result.Errors);
 		}
-	}
+
+        [HttpGet]
+        [Route("api/users/byrole")]
+        public async Task<IActionResult> GetUsersByRole(string roleName)
+        {
+            var users = new List<UserDto>();
+
+            // Chaîne de connexion à la base de données
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            // Requête SQL
+            string query = @"
+        SELECT u.Id, u.UserName, u.Email, r.Name AS RoleName
+        FROM AspNetUsers u
+        INNER JOIN AspNetUserRoles ur ON u.Id = ur.UserId
+        INNER JOIN AspNetRoles r ON ur.RoleId = r.Id
+        WHERE r.Name = @RoleName";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Ajout du paramètre SQL pour éviter les injections SQL
+                    command.Parameters.AddWithValue("@RoleName", roleName);
+
+                    connection.Open();
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            users.Add(new UserDto
+                            {
+                                Id = reader["Id"].ToString(),
+                                UserName = reader["UserName"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                RoleName = reader["RoleName"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return Ok(users);
+        }
+
+    }
 }
